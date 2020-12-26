2b7718d0244737b1b9a062fab63125d80fceaf9a
Kabykabir <33374362+Kabykabir@users.noreply.github.com>
2020-12-26 18:31:36 +0530

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc346d3dc290b001ef25e29",
+				"5fc346d3dc290b001ef25e2b"
+			]
+		},
+		"5fc346d3dc290b001ef25e29": {
+			"id": "5fc346d3dc290b001ef25e29",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc346d3dc290b001ef25e2b": {
+			"id": "5fc346d3dc290b001ef25e2b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
